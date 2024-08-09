912b5dc2bfc1967bf6ccaa8824605d15593cf1b5
unknown <mariajosemurciamartinez85@gmail.com>
2024-08-09 16:42:50 -0500

    Se sube

---
+++ EjerciciosEvaluacion/EjecicioPersona/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjecicioPersona
+{
+    //Clase Empleado que herede de Persona e incluya un
+    //atributo adicional Salario. Sobreescribe el método
+    //para mostrar la información, incluyendo el salario
+    public class Empleado
+    {
+    }
+}
---
+++ EjerciciosEvaluacion/EjecicioPersona/Persona.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjecicioPersona
+{
+    //Clase persona con atributos Nombre, Edad, y Direccion.
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Direccion { get; set; }
+
+        //Constructor que inicializa los atributos
+        public Persona(string nombre, int edad, string direccion)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Direccion = direccion;
+
+        }
+        //Constructor sin parámetros que asigne valores predeterminados a los atributos
+        public Persona()
+        {
+            Nombre = "";
+            Edad = 0;
+            Direccion = "";
+
+        }
+        //Metodo para mostrar la informacón de la persona
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Edad: {Edad}");
+            Console.WriteLine($"Direccion: {Direccion}");
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //Persona usando el constructor con parámetros
+                Persona persona1 = new Persona("Maria Jose", 18, "Carrera 13B Número 29-23 Las Palmitas Campoalegre");
+                persona1.MostrarInformacion();
+
+                Console.WriteLine();
+
+                //Método a la clase Persona que muestre la información de la persona en la consola 
+                Persona persona2 = new Persona();
+                persona2.MostrarInformacion();
+            }
+        }
+    }
+}
